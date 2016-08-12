7ea2e360dcbcb5c84b26310ad22e3ce2bc4f0c19
Dean Scelza <deanscelza@gmail.comgit config --global user.email deanscelza@gmail.com>
2016-08-12 16:36:08 -0700

    attempt to fix issue with gh-pages

---
+++ README.md
@@ -0,0 +1,21 @@
+# Track-Suggester #
+
+#### Bootstrap, Javascript & jQuery-powered Epicodus Track Suggester ####
+
+## Week 2 Independent Project by Dean Scelza @ Epicodus, 8-12-16 ##
+
+### Please check out my work on gh-pages: https://d34n5.github.io/track-suggester
+
+## Description & Technologies Used
+This webpage helps aspiring coding students choose a programming language to study in detail.  Users are surveyed for their preferences, and a suggested track is returned.  Project also provides me an opportunity to exercise my new Bootstrap, Javascript & jQuery skills.
+
+## Installation & Set-Up
+1. Clone the repository: https://github.com/d34n5/track-suggester
+2. Use your preferred browser to open index.html
+
+## Known Bugs
+There are no known bugs.  However, I encourage both feedback and criticism: If you have ANY concerns, questions or suggestions, feel free to contact me at:  deanscelza@gmail.com
+
+## License
+**Copyright (c) 2016, Dean Scelza @ Epicodus**
+This software is licensed under the MIT license.
---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+  $("#epicodus").submit(function(event) {
+    var firstNameInput = $("input#firstName").val();
+    var environmentInput = $("input:radio[name=environment]:checked").val();
+    var companyInput = $("input:radio[name=company]:checked").val();
+    var contentInput = $("#content").val();
+    var usersInput = $("#users").val();
+    var foodInput = $("#food").val();
+    $(".firstName").text(firstNameInput);
+    // --- ^^Business Logic^^ --- vvUser Logicvv ---
+    if (environmentInput === "front") {
+      $("#cssMessage").fadeIn("slow");
+    }
+    if (environmentInput != "front" && companyInput === "large") {
+      $("#cSharpMessage").fadeOut("slow");
+    } else if (environmentInput != "front" && companyInput === "small") {
+      $("#javaMessage").slideUp("slow");
+    }
+    if (environmentInput != "front" && foodInput === "Pizza") {
+      alert("We suggest taking the PHP/Drupal track at Epicodus.")
+    } else if (environmentInput != "front" && companyInput != "small" &&
+      foodInput === "BBQ") {
+      $("#cSharpMessage").slideDown("slow");
+    } else {
+      $("#javaMessage").fadeIn("slow");
+    }
+    event.preventDefault();
+  });
+});
